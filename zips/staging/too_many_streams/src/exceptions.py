2f3db3462e343cdd78eb9f33c72cd896dc1098f0
JamesWRC <James.cockshott@pm.me>
2025-09-22 18:35:45 +1000

    rm staging data

--- zips/staging/too_many_streams/src/exceptions.py
+++
@@ -1,9 +0,0 @@
-# Custom exception for the TooManyStreams plugin
-
-class TooManyStreamsException(Exception):
-    """Base exception for TooManyStreams plugin"""
-    pass
-
-class TMS_CustomStreamNotFound(TooManyStreamsException):
-    """Raised when a custom stream is not found"""
-    pass
